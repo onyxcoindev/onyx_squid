7b6c5ccc71b0d132b85ce2384d4f3ea4838d9f63
onyxcoindev <onyxcoindev>
2025-07-29 15:27:42 +0700

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'onyx-squid-processor',
+      script: 'sqd process',
+      watch: false,
+      max_memory_restart: '1G',
+    },
+    {
+      name: 'onyx-squid-cron',
+      script: 'sqd cron',
+      watch: false,
+      max_memory_restart: '1G',
+    },
+    {
+      name: 'onyx-squid-server',
+      script: 'sqd serve',
+      watch: false,
+      max_memory_restart: '1G',
+    },
+  ],
+};
