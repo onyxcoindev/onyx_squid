d0bd79385397d9ce169ecb4b179ba7ac7201ccd8
onyxcoindev <onyxcoindev>
2025-08-18 14:41:09 +0700

    add pm2 env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,18 +5,36 @@ module.exports = {
       script: 'sqd process:prod',
       watch: false,
       max_memory_restart: '1G',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: 'development',
+      },
     },
     {
       name: 'onyx-squid-cron',
       script: 'sqd cron',
       watch: false,
       max_memory_restart: '1G',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: 'development',
+      },
     },
     {
       name: 'onyx-squid-server',
       script: 'sqd serve:prod',
       watch: false,
       max_memory_restart: '1G',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: 'development',
+      },
     },
   ],
-};
+}
